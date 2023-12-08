ad45e1713967f1214a0623aa933dff47dc2fed6c
machii0601 <144580610+machii0601@users.noreply.github.com>
2023-12-08 09:23:36 +0000

    フォローmigration

--- db/migrate/20231208083518_create_follows.rb
+++ db/migrate/20231208083518_create_follows.rb
@@ -1,7 +1,8 @@
 class CreateFollows < ActiveRecord::Migration[6.1]
   def change
     create_table :follows do |t|
-
+      t.references :following, foreign_key: {to_table: :users}, null: false
+      t.references :followed, foreign_key: {to_table: :users}, null: false
       t.timestamps
     end
   end
