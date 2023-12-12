013d726718184f966b7914870843370db5026e18
machii0601 <144580610+machii0601@users.noreply.github.com>
2023-12-12 00:44:12 +0000

    admin/usersコントローラとルーティング設定

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,6 +3,15 @@ class Admin::UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+  end
+
+  def hide
+    @user = User.find(params[:id])
+    @user.update(is_deleted: true)
+    reset_session
+    flash[:notice] = "ありがとうございました。またのご利用をお待ちしております。"
+    redirect_to root_path
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   }
   namespace :admin do
   resources :users, only: [:index, :show, :edit]
+  put "/admin/users/:id/hide" => "users#hide", as: 'users_hide'
 end
 
 scope module: :user do
