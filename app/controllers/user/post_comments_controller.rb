71d2b26fd018dab65daeac9bd64f81a453220a83
machii0601 <144580610+machii0601@users.noreply.github.com>
2023-12-12 01:20:13 +0000

    コメント機能作成

--- app/controllers/user/post_comments_controller.rb
+++ app/controllers/user/post_comments_controller.rb
@@ -1,4 +1,18 @@
 class User::PostCommentsController < ApplicationController
+
+  def create
+    post = Post.find(params[:post_id])
+    comment = current_user.post_comments.new(post_comment_params)
+    comment.post_id = post.id
+    comment.save
+    redirect_to post_path(post)
+  end
+
+  def destroy
+    PostComment.find(params[:id]).destroy
+    redirect_to post_path(params[:post_id])
+  end
+
   def new
   end
 
@@ -6,5 +20,11 @@ class User::PostCommentsController < ApplicationController
   end
 
   def show
+  end
+
+  private
+
+  def  post_comment_params
+    params.require(:post_comment).permit(:comment)
   end
 end
