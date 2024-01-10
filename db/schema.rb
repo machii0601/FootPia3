da29bf31b2f5624a166eea42ac098abe69fd7aff
machii0601 <144580610+machii0601@users.noreply.github.com>
2024-01-10 07:23:08 +0000

    通知機能maigrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_06_121308) do
+ActiveRecord::Schema.define(version: 2024_01_10_071957) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -38,6 +38,18 @@ ActiveRecord::Schema.define(version: 2024_01_06_121308) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "activities", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "subject_type"
+    t.integer "subject_id"
+    t.integer "action_type", null: false
+    t.boolean "read", default: false, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["subject_type", "subject_id"], name: "index_activities_on_subject"
+    t.index ["user_id"], name: "index_activities_on_user_id"
   end
 
   create_table "admins", force: :cascade do |t|
@@ -163,6 +175,7 @@ ActiveRecord::Schema.define(version: 2024_01_06_121308) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "activities", "users"
   add_foreign_key "follows", "users"
   add_foreign_key "follows", "users", column: "follow_id"
   add_foreign_key "tag_maps", "posts"
